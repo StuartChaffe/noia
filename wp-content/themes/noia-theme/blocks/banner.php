1977d6d41a826efe50013f1984428f8415d5785d
Stuart Chaffe <stu@stuartchaffe.co.uk>
2021-08-13 12:56:59 +0100

    Add logo option

--- wp-content/themes/noia-theme/blocks/banner.php
+++ wp-content/themes/noia-theme/blocks/banner.php
@@ -3,6 +3,7 @@
 * The template used for displaying a banner block.
 */
 $image = get_field( 'banner-image');
+$logo = get_field( 'banner-logo');
 $text = get_field( 'banner-text', false, false);
 $link = get_field( 'banner-background-link');
 $bkgimage = get_field( 'banner-background-image');
@@ -21,6 +22,10 @@ $overlay = get_field( 'banner-background-overlay');
 	</div>
 <?php } ?>
 
+<?php if ( $logo ) { ?>
+	<img class="banner--logo" src="<?php echo $logo['url']; ?>" alt="<?php echo $logo['url']; ?>" />
+<?php } ?>
+	
 <?php if ( $text ) { ?>
 	<?php if ( $link ) { ?>
 	<a href="<?php echo $link['url'] ?>" class="banner--link">
