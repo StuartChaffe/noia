278df443b564a338fc6859970a73dbb8c10ad6ba
Stuart Chaffe <stu@stuartchaffe.co.uk>
2021-08-17 14:45:43 +0100

    Add SVG support

--- wp-content/themes/noia-theme/functions.php
+++ wp-content/themes/noia-theme/functions.php
@@ -20,3 +20,12 @@ require_once( 'lib/vendors.php' );
 
 // Helper functions
 require_once( 'lib/helpers.php' );
+
+
+function add_file_types_to_uploads($file_types){
+    $new_filetypes = array();
+    $new_filetypes['svg'] = 'image/svg+xml';
+    $file_types = array_merge($file_types, $new_filetypes );
+    return $file_types;
+    }
+    add_filter('upload_mimes', 'add_file_types_to_uploads');
