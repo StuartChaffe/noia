74569cf3e0879dae7b93c80fee81a1ae797c689e
Stuart Chaffe <stu@stuartchaffe.co.uk>
2021-08-19 12:14:49 +0100

    Update header icon

--- wp-content/themes/noia-theme/partials/global-header.php
+++ wp-content/themes/noia-theme/partials/global-header.php
@@ -1,7 +1,16 @@
 <header class="global-header__menu">
 	<button class="global-header__menu-btn" data-toggle="#mobile-menu" data-toggle-body-class="nav-open" aria-label="Toggle mobile menu" aria-expanded="false" aria-controls="mobile-menu">
 		<span class="global-header--nav-box">
-			<span class="global-header--nav-inner"><span>Mobile Menu</span></span>
+		<span class="global-header--nav-logo"><svg width="22" height="28" viewBox="0 0 22 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path d="M22 28H19.9048V1.86667H2.09524V28L0 28V0H22V28Z" fill="white"/>
+		</svg></span>
+
+		<span class="global-header--nav-close"><svg width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<path d="M14.1714 18.4142L15.5856 19.8284L9.22168 26.1924L7.80747 24.7782L14.1714 18.4142Z" fill="white"/>
+		<path d="M9.22168 7.80761L7.80747 9.22183L24.778 26.1924L26.1922 24.7782L18.4141 17L26.1922 9.22183L24.778 7.80761L16.9999 15.5858L9.22168 7.80761Z" fill="white"/>
+		</svg>
+		</span>
+		
 		</span>
 	</button>
 	<div class="mobile-menu" id="mobile-menu">
