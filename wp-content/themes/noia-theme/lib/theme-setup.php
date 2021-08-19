92b072f2292f96cf9047dd1ba91646c23fb1fa87
Stuart Chaffe <stu@stuartchaffe.co.uk>
2021-08-19 13:52:22 +0100

    Change hover

--- wp-content/themes/noia-theme/lib/theme-setup.php
+++ wp-content/themes/noia-theme/lib/theme-setup.php
@@ -9,6 +9,17 @@ add_theme_support( 'disable-custom-font-sizes' );
 
 
 
+//Page Slug Body Class
+function add_slug_body_class( $classes ) {
+global $post;
+if ( isset( $post ) ) {
+$classes[] = $post->post_type . '-' . $post->post_name;
+}
+return $classes;
+}
+add_filter( 'body_class', 'add_slug_body_class' );
+
+
 /**
  * Tidy up head
  */
