d797e04a8471f9c722642595fd2e30c90cd5bec0
Stuart Chaffe <stu@stuartchaffe.co.uk>
2021-09-21 12:03:03 +0100

    Update arrows

--- wp-content/themes/noia-theme/blocks/cta.php
+++ wp-content/themes/noia-theme/blocks/cta.php
@@ -18,14 +18,14 @@ $bkgvideo = get_field( 'cta-background-video');
 	<div class="cta--links">
 	<?php if ( $linkleft ) { ?>
 		<div class="cta--links-item">
-			<a href="<?php echo $linkleft['url']; ?>"><p class="animate heading-1"><?php echo $linkleft['title']; ?></p> <div class="arrow"><svg width="100" height="47" viewBox="0 0 150 47" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<a href="<?php echo $linkleft['url']; ?>"><p class="heading-1"><?php echo $linkleft['title']; ?></p> <div class="arrow"><svg width="100" height="31" viewBox="0 0 150 47" fill="none" xmlns="http://www.w3.org/2000/svg">
 			<path d="M150 23.5L110 0.405989V46.594L150 23.5ZM0 27.5H114V19.5H0V27.5Z" fill="currentColor"></path>
 		</svg></div></a>
 		</div>
 	<?php } ?>
 	<?php if ( $linkright ) { ?>
 		<div class="cta--links-item">
-			<a href="<?php echo $linkright['url']; ?>"><p class="animate heading-1"><?php echo $linkright['title']; ?></p> <div class="arrow"><svg width="100" height="47" viewBox="0 0 150 47" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<a href="<?php echo $linkright['url']; ?>"><p class="heading-1"><?php echo $linkright['title']; ?></p> <div class="arrow"><svg width="100" height="31" viewBox="0 0 150 47" fill="none" xmlns="http://www.w3.org/2000/svg">
 			<path d="M150 23.5L110 0.405989V46.594L150 23.5ZM0 27.5H114V19.5H0V27.5Z" fill="currentColor"></path>
 		</svg></div></a>
 		</div>
