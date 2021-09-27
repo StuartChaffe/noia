10da18809e3b5b4be09d71e6a490e99bbd30d68f
Stuart Chaffe <stu@stuartchaffe.co.uk>
2021-09-27 13:00:06 +0100

    Update manifesto HTML for image switch

--- wp-content/themes/noia-theme/src/js/components/manifesto.js
+++ wp-content/themes/noia-theme/src/js/components/manifesto.js
@@ -1,7 +1,7 @@
 jQuery(function ($) {
 	$(window).on('scroll', function () {
 		$('.manifesto-item').each(function () { 
-			var distance = $(this).offset().top;
+			var distance = $(this).offset().top - 200;
 			if ($(window).scrollTop() >= distance) {
 				var image = $(this).attr('data-sticky-image');
 				$("#manifesto-image").attr("src", image);
