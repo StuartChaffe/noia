aea3f9b69b0723bea0eaf4b3b151433da9ac060b
Stuart Chaffe <stu@stuartchaffe.co.uk>
2021-09-28 10:40:11 +0100

    Add content and style featherlight

--- wp-content/themes/noia-theme/partials/global-contact.php
+++ wp-content/themes/noia-theme/partials/global-contact.php
@@ -4,33 +4,79 @@
 <a href="#part-4" class="ajax-featherlight">Part 4</a>
 
 <div class="global-modal" id="part-1">
-	<div class="modal__inner">
-		<p>Modal one content</p>
-		<a href="#part-2" class="ajax-featherlight">Part 2</a>
-		<a href="#part-3" class="ajax-featherlight">Part 3</a>
-		<a href="#part-4" class="ajax-featherlight">Part 4</a>
+	<div class="modal-inner">
+		<div class="modal-inner-item">
+			<h3>Stay with <br />Noiascape</h3>
+			<p>Tell us about you, where you want to stay, what you<br /> need, or just ask a general question.</p>
+			<div class="modal-inner-item--buttons">
+				<a href="#part-2" class="btn ajax-featherlight">Stay with us</a> <a href="#part-3" class="btn ajax-featherlight">Live with us</a>
+			</div>
+		</div>
+		<div class="modal-inner-item">
+			<h3>Collaborate with <br />Noiascape</h3>
+			<p>Use our social spaces as a way to find your<br /> audience and contribute to your local culture.</p>
+			<div class="modal-inner-item--buttons">
+				<a href="#part-4" class="btn ajax-featherlight">Tell us about your idea</a>
+			</div>
+		</div>
 	</div>
 </div>
 
 <div class="global-modal" id="part-2">
-	<div class="modal__inner">
-		<p>Modal two content</p>
-		<a href="#part-1" class="ajax-featherlight">Back</a>
-	</div>
+	<div class="modal-inner">
+		<a href="#part-1" class="btn-back ajax-featherlight">Back</a>
+		<div class="modal-inner-item">
+			<h3>Stay with Noiascape</h3>
+			<?php echo do_shortcode('[contact-form-7 id="683" title="Stay with Noiascape"]'); ?>
+		</div>
+	</div>	
 </div>
 
 
 <div class="global-modal" id="part-3">
-	<div class="modal__inner">
-		<p>Modal three content</p>
-		<a href="#part-1" class="ajax-featherlight">Back</a>
+	<div class="modal-inner">
+		<a href="#part-1" class="btn-back ajax-featherlight">Back</a>
+		<div class="modal-inner-item">
+			<h3>Live with <br />Noiascape</h3>
+			<p>Tell us about you, where you want to stay, what you<br /> need, or just ask a general question.</p>
+
+			3RD PARTY FORM 
+			<div class="modal-inner-item--buttons">
+				<a href="#part-5" class="btn ajax-featherlight">Submit</a>
+			</div>
+		</div>
 	</div>
 </div>
 
 
 <div class="global-modal" id="part-4">
-	<div class="modal__inner">
-		<p>Modal four content</p>
-		<a href="#part-1" class="ajax-featherlight">Back</a>
+	<div class="modal-inner">
+		<a href="#part-1" class="btn-back ajax-featherlight">Back</a>
+
+		<div class="modal-inner-item">
+			<h3>Collaborate with <br />Noiascape</h3>
+			<p>Use our social spaces as a way to find your audience and contribute to your local culture.</p>
+
+			FORM 
+			<div class="modal-inner-item--buttons">
+				<a href="#part-5" class="btn ajax-featherlight">Submit</a>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<div class="global-modal" id="part-5">
+	<div class="modal-inner">
+		<a href="#part-1" class="btn-back ajax-featherlight">Start again</a>
+		<div class="modal-inner-item">
+			<p>Enquiry successfully submitted</p>
+			<h3>Thanks for contacting us.<br />
+			<span>We’ll get back to you within 24 hours</span></h3>
+
+			<div class="modal-inner-item--buttons">
+				<button class="featherlight-close btn btn--primary" aria-label="Close">Back to Noiascape</button>
+			</div>
+		</div>
 	</div>
 </div>
--- wp-content/themes/noia-theme/partials/global-footer.php
+++ wp-content/themes/noia-theme/partials/global-footer.php
@@ -5,7 +5,7 @@
 $logo = get_field( 'logo-footer', 'options');
 ?>
 
-<a href="#" class="ask" data-toggle="modal" data-target="#contact"><img src="<?php echo get_template_directory_uri(); ?>/src/images/ask.svg" alt="ask Logo" /></a>
+<a href="#part-1" class="ask ajax-featherlight"><img src="<?php echo get_template_directory_uri(); ?>/src/images/ask.svg" alt="ask Logo" /></a>
 <?php get_template_part( 'partials/global-contact' ); ?>
 <footer class="global-footer">
 	<div class="global-footer--logo">
