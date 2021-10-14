607fa5a582b8f610ebf97ba69c46361fcdac08c7
Stuart Chaffe <stu@stuartchaffe.co.uk>
2021-10-14 16:25:05 +0100

    Add content to space cta

--- wp-content/themes/noia-theme/blocks/space-cta.php
+++ wp-content/themes/noia-theme/blocks/space-cta.php
@@ -4,13 +4,13 @@
 */
 $bkgimage = get_field( 'spacecta-background-image');
 $content = get_field( 'spacecta-content');
+$button = get_field( 'spacecta-button');
 ?>
 
 <section class="space-cta" <?php if ( $bkgimage ) { ?>style="background-image: url(<?php echo $bkgimage['url']; ?>)"<?php } ?>>
 	<div class="space-cta--content">
-		<h3>Interested in living at High Street House? Just ASK</h3>
-		<p>We currently have availabilty for upcoming long and short stay memberships</p>
+		<?php echo $content ?>
 
-		<a href="#part-3" class="ajax-featherlight btn">Long stay - Enquiries</a> <a href="#part-2" class="ajax-featherlight btn">Short stay - Availability</a>
+		<a href="#part-2" class="btn ajax-featherlight"><?php if ( $button ) { ?><?php echo $button ?><?php } else { ?>Stay with us<?php } ?></a>
 	</div>
 </section>
