714da1b543348f5f78214a86d01b68949a2deda7
Stuart Chaffe <stu@stuartchaffe.co.uk>
2021-11-01 12:13:03 +0000

    Update addthis

--- wp-content/themes/noia-theme/footer.php
+++ wp-content/themes/noia-theme/footer.php
@@ -3,6 +3,6 @@
 </div><!-- /.global-container -->
 <?php wp_footer(); ?>
 
-<!-- Go to www.addthis.com/dashboard to customize your tools --> <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5aafbf38378f22eb"></script>
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-617fd74ceeb58537"></script>
 </body>
 </html>
