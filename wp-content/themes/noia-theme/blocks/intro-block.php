3d11a8c8288253e3f7a03798049f6deac2463030
Stuart Chaffe <stu@stuartchaffe.co.uk>
2021-11-01 15:07:14 +0000

    Add background and text colour

--- wp-content/themes/noia-theme/blocks/intro-block.php
+++ wp-content/themes/noia-theme/blocks/intro-block.php
@@ -3,15 +3,17 @@
 * The template used for displaying an intro text block.
 */
 $bkgcolor = get_field( 'bkgcolor');
+$bkgimage = get_field( 'bkgimage');
 $text = get_field('textblock-text');
 $content = $text['textblock-content'];
+$color = $text['textblock-text-colour'];
 $size = $text['textblock-text-size'];
 ?>
 
-<section class="intro-block text-block <?php echo $bkgcolor ?> <?php echo $size ?>">
+<section class="intro-block text-block <?php echo $bkgcolor ?> <?php echo $size ?>" <?php if ( $bkgimage ) { ?>style="background-image: url(<?php echo $bkgimage['url']; ?>)"<?php } ?>>
 	<div class="<?php echo $size ?>">
 		<div class="text-block--text-inner">
-			<div class="text-block--text__content">
+			<div class="text-block--text__content <?php echo $color ?>">
 				<?php echo $content ?>
 			</div>
 		</div>
