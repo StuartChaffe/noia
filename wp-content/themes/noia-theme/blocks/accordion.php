4cd4040062060d4b7b0e88607654f88ceed50013
Stuart Chaffe <stu@stuartchaffe.co.uk>
2021-12-07 13:53:39 +0000

    Move open icon

--- wp-content/themes/noia-theme/blocks/accordion.php
+++ wp-content/themes/noia-theme/blocks/accordion.php
@@ -32,13 +32,13 @@ $counter = 1;
 		?>
 		<div class="accordion-item <?php echo $i ?>" style="background-image:url(<?php echo $image['url']; ?>);">
 			<div class="accordion-item--header">
-				<div class="accordion-item--open"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-						<path d="M9 11V13H0L8.74243e-08 11H9Z" fill="currentColor"/>
-						<path d="M13 0H11V24H13V13H24V11L13 11V0Z" fill="currentColor"/>
-					</svg>
-				</div>
 				<div class="accordion-item--number"><p>0<?php echo $i ?></p></div>
 				<div class="accordion-item--title"><p>0<?php echo $i ?> — <?php echo $title ?></p></div>
+			</div>
+			<div class="accordion-item--open"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path d="M9 11V13H0L8.74243e-08 11H9Z" fill="currentColor"/>
+					<path d="M13 0H11V24H13V13H24V11L13 11V0Z" fill="currentColor"/>
+				</svg>
 			</div>
 			<!-- <div class="show-content">[Read]</div> -->
 			<div class="accordion-item--content">
