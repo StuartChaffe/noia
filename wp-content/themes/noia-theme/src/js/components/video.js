2491b1ae3ad0d59b7827ff3d3d7fd39317e88ddc
Stuart Chaffe <stu@stuartchaffe.co.uk>
2021-12-10 12:44:25 +0000

    Update video icons

--- wp-content/themes/noia-theme/src/js/components/video.js
+++ wp-content/themes/noia-theme/src/js/components/video.js
@@ -1,9 +1,9 @@
 jQuery(function ($) {
 	$("video").prop('muted', true);
 
-	$("video").click( function (){
-		$(this).prop('muted', !$(this).prop('muted'));
-	});
+	// $("video").click( function (){
+	// 	$(this).prop('muted', !$(this).prop('muted'));
+	// });
 
 	$(".banner--video__button-desktop").click( function (){
 		$(".banner--video-desktop").prop('muted', !$(".banner--video-desktop").prop('muted'));
@@ -16,6 +16,9 @@ jQuery(function ($) {
 		$(this).toggleClass('unmute');
 
 		$(".banner--logo").toggleClass('hide');
+	});
+	$( ".button-replay" ).click(function() {
+		$('#video-desktop').get(0).currentTime = 0;
 	});
 	
 });
