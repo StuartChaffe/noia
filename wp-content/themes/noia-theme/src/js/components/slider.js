b0e9dfe9d399e62b1b86f5bb02abc6e416daee07
Stuart Chaffe <stu@stuartchaffe.co.uk>
2021-12-17 09:23:25 +0000

    Update carousel speed

--- wp-content/themes/noia-theme/src/js/components/slider.js
+++ wp-content/themes/noia-theme/src/js/components/slider.js
@@ -5,7 +5,7 @@ jQuery(document).ready(function($) {
 		slidesToShow: 1,
 		slidesToScroll: 1,
 		autoplay: true,
-		autoplaySpeed: 2000,
+		autoplaySpeed: 5000,
 		arrows: true,
 		infinite: true,
 		dots: false,
