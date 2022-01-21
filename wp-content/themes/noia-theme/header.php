3beaf2639c7b5bfcc002aec34d45abe855b207b5
Stuart Chaffe <stu@stuartchaffe.co.uk>
2022-01-21 14:30:43 +0000

    Add GA code

--- wp-content/themes/noia-theme/header.php
+++ wp-content/themes/noia-theme/header.php
@@ -9,6 +9,16 @@
 <link rel="icon" href="/favicon.svg" type="image/svg+xml">
 <link rel="apple-touch-icon" href="/apple-touch-icon.png"><!-- 180×180 -->
 <?php wp_head(); ?>
+
+<!-- Google Analytics -->
+<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', 'UA-100645214-1', 'auto');
+	ga('set', 'anonymizeIp', true);
+	ga('send', 'pageview');</script>
+	<!-- End: Google Analytics -->
 </head>
 <body <?php body_class(); ?>>
 <div id="svg-sprite" style="height: 0; width: 0; position: absolute; visibility: hidden">
